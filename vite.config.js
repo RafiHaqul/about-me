1f6fcd13c7f98c683e27ec1738ba3cd00f2b0c43
RafiHaqul <rahabarafiel@gmail.com>
2026-07-20 10:16:08 +0800

    refactor: hompage retry 3

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/about-me/',
   server: {
     host: true, // atau isi dengan IP spesifik Anda, misal: '192.168.1.100'
-    base: "https://rafihaqul.github.io/about-me"
   }
 
 })
