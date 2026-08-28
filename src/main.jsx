68e526e66f2f57f54b8d28baec268a40b32705d2
RafiHaqul <rahabarafiel@gmail.com>
2026-08-28 21:43:55 +0800

    refactor: Homepages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './App.css'
 
+if (window.location.pathname === '/') {
+  window.location.replace(`/about-me/${window.location.hash}`)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/about-me">
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/about-me/',
+  base: '/',
   server: {
     host: true, // atau isi dengan IP spesifik Anda, misal: '192.168.1.100'
   }
